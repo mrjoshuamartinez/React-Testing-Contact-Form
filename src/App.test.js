7728957d2d5625cf9363f24e64ccba3999f12c4e
Joshua Martinez <mrjoshuamartinez@gmail.com>
2020-10-08 08:12:32 -0400

    Completed MVP

--- src/App.test.js
+++ src/App.test.js
@@ -1,31 +1,27 @@
 import React from "react";
-import { render, fireEvent, screen } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import App from "./App";
+import ContactForm from './components/ContactForm';
 
-describe('App', () => {
-  it('Mounts to a simulated DOM', () => {
-    // render the component
-    const simulatedDOM = render(<App />);
-    // query the simulated DOM for labels containing 'Name*'
-    console.log(simulatedDOM);
-  });
-  it('User can fill out form properly.', () => {
-    // Render the component
-    render(<App />);
-    // Query the virtual DOM for each input field, so we can work with them
-    const firstName = screen.getByText(/First Name/i);
-    const lastName = screen.getByText(/Last Name/i);
-    const email = screen.getByText(/Email/i);
-    const message = screen.getByText(/Message/i);
-    // Fill out user form
-    fireEvent.change(firstName, {target: {value: "Joshua"}});
-    fireEvent.change(lastName, {target: {value: "Martinez"}});
-    fireEvent.change(email, {target: {value: "mrjoshuamartinez@gmail.com"}});
-    fireEvent.change(message, {target: {value: "Hello World"}});
-    // Sanity check: assert that the form inputs contain the values we typed in
-    expect(firstName).toHaveValue("Joshua");
-    expect(lastName).toHaveValue("Martinez");
-    expect(email).toHaveValue("mrjoshuamartinez@gmail.com");
-    expect(message).toHaveValue("Hellow World");
-  });
+test(" form renders without errors", () => {
+  render(<ContactForm />);
+})
+
+test("all input fields are working", async () => {
+  const { getByLabelText } = render(<ContactForm />);
+  const firstNameInput = getByLabelText(/first name*/i);
+  const message = getByLabelText(/message*/i);
+  const lastNameInput = getByLabelText(/last name*/i);
+  const emailInput = getByLabelText(/Email/i);
+  expect(lastName).toBeTruthy();
+  expect(firstName).toBeTruthy();
+  expect(email).toBeTruthy();
+  expect(message).toBeTruthy();
+  fireEvent.change(firstNameInput,{target: {name:'firstName', value:'Joshua'}});
+  fireEvent.change(lastNameInput,{target: {name:'lastName', value:'Martinez'}});
+  fireEvent.change(emailInput,{target: {name:'email', value:'mrjoshuamartinez@gmail.com'}});
+}) 
+
+test("renders App without crashing", () => {
+  render(<App />);
 });
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
+
 const ContactForm = () => {
+
   const [data, setData] = useState();
   const { register, errors, handleSubmit } = useForm({
     mode: "onBlur",
@@ -11,53 +13,62 @@ const ContactForm = () => {
   };
 
   return (
+
     <div className="App">
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label htmlFor="firstName">First Name*</label>
           <input
             name="firstName"
-            placeholder="Edd"
-            ref={register({ required: true, maxLength: 20 })}
+            id="firstName"
+            placeholder="Joshua"
+            ref={register({ required: true, minLength: 3 })}
           />
           {errors.firstName && (
             <p>Looks like there was an error: {errors.firstName.type}</p>
           )}
         </div>
-
         <div>
           <label htmlFor="lastName">Last Name*</label>
           <input
             name="lastName"
-            placeholder="Burke"
+            id="lastName"
+            placeholder="Martinez"
             ref={register({ required: true })}
           />
           {errors.lastName && (
             <p>Looks like there was an error: {errors.lastName.type}</p>
           )}
         </div>
-
         <div>
-          <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
+          <label htmlFor="email"
+            placeholder="mrjoshuamartinez@gmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input
+            name="email"
+            id="email"
+            ref={register({ required: true })} />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea
+            name="message"
+            id="message"
+            ref={register({ required: false })} />
         </div>
         {data && (
           <pre style={{ textAlign: "left", color: "white" }}>
             {JSON.stringify(data, null, 2)}
           </pre>
         )}
-        <input type="submit" />
+        <input type="submit" id="submit" />
       </form>
     </div>
+
   );
 };
 
